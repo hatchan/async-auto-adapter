11c2696276eb2d47e9aa94c7f199d93449225f71
Benno van den Berg <bennovandenberg@gmail.com>
2013-05-10 17:04:27 +0200

    Add support for mapped values

--- spec/async-auto-adapter-specs.js
+++ spec/async-auto-adapter-specs.js
@@ -9,7 +9,7 @@ if (typeof require === "function" && typeof module === "object") {
 
 buster.spec.expose();
 
-var spec = describe("async-auto-adapter", function(){
+var spec = describe("async-auto-adapter", function() {
     "use strict";
 
     describe("when given a single function", function() {
@@ -108,4 +108,87 @@ var spec = describe("async-auto-adapter", function(){
             expect(this.funcImpl).toHaveBeenCalledWithExactly(callbackMethod);
         });
     });
+
+    describe("when given a two dependencies in a string, a single map and a function", function() {
+        before(function() {
+            this.dependency1 = "dep1";
+            this.dependency2 = "dep2";
+            this.dependencies = "dep1 dep2";
+            this.map = "dep1";
+            this.funcImpl = this.spy();
+            this.result = adapter(this.dependencies, this.map, this.funcImpl);
+        });
+
+        it("the result should be a array", function() {
+            expect(this.result).toBeArray();
+        });
+
+        it("the results should contain 3 items", function() {
+            expect(this.result.length).toEqual(3);
+        });
+
+        it("the result should contain the first dependency as the first item", function() {
+            expect(this.result[0]).toEqual(this.dependency1);
+        });
+
+        it("the result should contain the second dependency as the second item", function() {
+            expect(this.result[1]).toEqual(this.dependency2);
+        });
+
+        it("the result should contain a function as the last item", function() {
+            expect(this.result[2]).toBeFunction();
+        });
+
+        it("this.funcImpl should be called with 1 parameter when calling the method in result", function() {
+            var dep1Value = "dep1Value";
+            var dep2Value = "dep2Value";
+            var callbackMethod = function() { console.log("callbackMethod"); };
+            this.result[2](callbackMethod, { "dep1": dep1Value, "dep2": dep2Value });
+
+            expect(this.funcImpl).toHaveBeenCalledOnce();
+            expect(this.funcImpl).toHaveBeenCalledWithExactly(dep1Value, callbackMethod);
+        });
+    });
+
+    describe("when given a two dependencies in a string, a mutliple maps and a function", function() {
+        before(function() {
+            this.dependency1 = "dep1";
+            this.dependency2 = "dep2";
+            this.dependencies = "dep1 dep2";
+            this.map = "dep2 dep1";
+            this.funcImpl = this.spy();
+            this.result = adapter(this.dependencies, this.map, this.funcImpl);
+        });
+
+        it("the result should be a array", function() {
+            expect(this.result).toBeArray();
+        });
+
+        it("the results should contain 3 items", function() {
+            expect(this.result.length).toEqual(3);
+        });
+
+        it("the result should contain the first dependency as the first item", function() {
+            expect(this.result[0]).toEqual(this.dependency1);
+        });
+
+        it("the result should contain the second dependency as the second item", function() {
+            expect(this.result[1]).toEqual(this.dependency2);
+        });
+
+        it("the result should contain a function as the last item", function() {
+            expect(this.result[2]).toBeFunction();
+        });
+
+        it("this.funcImpl should be called with 1 parameter when calling the method in result", function() {
+            var dep1Value = "dep1Value";
+            var dep2Value = "dep2Value";
+            var callbackMethod = function() { console.log("callbackMethod"); };
+            this.result[2](callbackMethod, { "dep1": dep1Value, "dep2": dep2Value });
+
+            expect(this.funcImpl).toHaveBeenCalledOnce();
+            expect(this.funcImpl).toHaveBeenCalledWithExactly(dep2Value, dep1Value, callbackMethod);
+        });
+    });
+
 });
---
+++ spec/mapper-specs.js
@@ -0,0 +1,46 @@
+var buster, adapter;
+if (typeof require === "function" && typeof module === "object") {
+    buster = require("buster");
+    adapter = require("../index");
+} else {
+    buster = window.buster;
+    adapter = window.adapter;
+}
+
+var spec = describe("mapper", function() {
+    "use strict";
+    describe("when given a property selector", function() {
+
+        it("should retrieve the value of property", function() {
+            var result = adapter.mapper("prop2", { "prop1": "val1", "prop2": "val2", "prop3": "val3" });
+            expect(result).toEqual("val2");
+        });
+
+    });
+
+    describe("when given a sub property selector", function() {
+
+        it("should retrieve the value of property", function() {
+            var result = adapter.mapper("prop2.subprop2", {
+              "prop1": { "subprop1": "val1.1", "subprop2": "val1.2", "subprop3": "val1.3" },
+              "prop2": { "subprop1": "val2.1", "subprop2": "val2.2", "subprop3": "val2.3" },
+              "prop3": { "subprop1": "val3.1", "subprop2": "val3.2", "subprop3": "val3.3" }
+            });
+            expect(result).toEqual("val2.2");
+        });
+
+    });
+
+    describe("when given a array selector", function() {
+
+        it("should retrieve the value of property", function() {
+            var result = adapter.mapper("prop2[1]", {
+              "prop1": [ "val1.1", "val1.2", "val1.3" ],
+              "prop2": [ "val2.1", "val2.2", "val2.3" ],
+              "prop3": [ "val3.1", "val3.2", "val3.3" ]
+            });
+            expect(result).toEqual("val2.2");
+        });
+
+    });
+});
